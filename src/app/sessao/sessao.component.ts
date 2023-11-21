9a8eb2824f4ceba6709a2a7b4e412a878dcbd4ed
luisfernandogaldino <gluisfernandoduarte@gmail.com>
2023-11-21 16:13:59 -0300

    ProjetoServicoReforco

--- src/app/sessao/sessao.component.ts
+++ src/app/sessao/sessao.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import Swal from 'sweetalert2';
+import{OnInit} from '@angular/core';
+import { SessaoService } from '../sessao/service/sessao.service';
+import { Isessao } from './service/isessao';
 
 @Component({
   selector: 'app-sessao',
   templateUrl: './sessao.component.html',
   styleUrls: ['./sessao.component.scss']
 })
-export class SessaoComponent {
-  Comprar(){
+export class SessaoComponent implements OnInit {
+ ngOnInit(): void {this.listar()}
+
+ produtos: Isessao[] = [];
+
+ constructor(private service: SessaoService){ }
+
+ listar(){
+  this.service.listarAll().subscribe(dados => this.produtos = dados);
+ }
+     
+Comprar(){
     Swal.fire({
       title: "Você deseja comparar esse produto?",
       showDenyButton: true,
@@ -16,7 +29,7 @@ export class SessaoComponent {
     }).then((result) => {
       /* Read more about isConfirmed, isDenied below */
       if (result.isConfirmed) {
-        Swal.fire("Salvo!", "", "success");
+        Swal.fire("Comprado com sucesso!", "", "success");
       } else if (result.isDenied) {
         Swal.fire("Erro", "", "error");
       }
